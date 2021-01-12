5593aa640ec786c3ba4306648f834e0901a1efef
Abid Hasan <abidhasan@gmail.com>
2021-01-11 17:52:00 -0800

    Add next js chakra scaffolded app

---
+++ roteit-web/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+
+const Index = () => (
+  <div>Hello</div>
+);
+
+export default Index;
